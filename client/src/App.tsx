4b3a0ffc68bc018491d27713c8d5c04e4687f76f
fanarali881-eng <252206377+fanarali881-eng@users.noreply.github.com>
2026-05-24 20:42:39 -0400

    Clean Slate: Removed all unused files to fix Netlify build

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,8 @@
-import { Switch, Route } from "wouter";
 import RechargePage from "./pages/RechargePage";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={RechargePage} />
-      <Route>404 Not Found</Route>
-    </Switch>
-  );
-}
 
 function App() {
   return (
-    <Router />
+    <RechargePage />
   );
 }
 
