01129524b4879fa4c161c5f6264bc14f1ce85c20
fanarali881-eng <252206377+fanarali881-eng@users.noreply.github.com>
2026-05-24 20:38:15 -0400

    Fix build errors and enable 100% mirror via iframe

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,18 @@
 import { Switch, Route } from "wouter";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lib/queryClient";
-import { Toaster } from "@/components/ui/sonner";
-import NotFound from "@/pages/not-found";
 import RechargePage from "@/pages/RechargePage";
-import SummaryPayment from "@/pages/SummaryPayment";
-import KNETPayment from "@/pages/KNETPayment";
-import SuccessPage from "@/pages/SuccessPage";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={RechargePage} />
-      <Route path="/summary-payment" component={SummaryPayment} />
-      <Route path="/knet-payment" component={KNETPayment} />
-      <Route path="/success" component={SuccessPage} />
-      <Route component={NotFound} />
+      <Route>404 Not Found</Route>
     </Switch>
   );
 }
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router />
-      <Toaster />
-    </QueryClientProvider>
+    <Router />
   );
 }
 
